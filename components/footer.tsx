b55b8a9b52a5ecb353b52f98956ec50efe517bda
lucilajuliana.jsx <lucilapastore@gmail.com>
2025-01-07 12:03:16 -0300

    fix: update footer content and remove unused link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
-import { motion } from "framer-motion";
 import { containerVariants, itemVariants } from "@/lib/animation-variants";
+import { motion } from "framer-motion";
 
 export default function Footer() {
   return (
@@ -10,16 +9,14 @@ export default function Footer() {
       animate="visible"
       className="mt-auto flex w-full items-center justify-center gap-1 border-t bg-background p-6 text-muted-foreground md:justify-start">
       <motion.div variants={itemVariants}>
-        Brought to you by{" "}
-        <Link
-          href="/about"
-          rel="noopener noreferrer"
-          target="_blank">
+        {/*Brought to you by{" "} */}Let&apos;s get back to the cypherpunk
+        roots!
+        {/*<Link href="/about" rel="noopener noreferrer" target="_blank">
           <span className="text-zinc-300 underline underline-offset-2 transition-all duration-200 ease-linear hover:text-yellow-200">
             Privacy LATAM
           </span>
           .
-        </Link>
+        </Link>*/}
       </motion.div>
     </motion.div>
   );
